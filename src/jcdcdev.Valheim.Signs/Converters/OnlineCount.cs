9a075d4bedefd6a4879545bcd136c362f2adc133
jcdcdev <james@jcdc.dev>
2024-09-01 23:58:41 +0100

    feature: online player count

---
+++ src/jcdcdev.Valheim.Signs/Converters/OnlineCount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace jcdcdev.Valheim.Signs.Converters
+{
+    public class OnlineCountSign : IAmADynamicSign
+    {
+        public bool CanConvert(Sign sign, string originalText) => originalText.Equals("onlinecount", StringComparison.InvariantCultureIgnoreCase);
+
+        public string? GetSignText(Sign sign, string originalText) => $"{ZNet.instance.GetPlayerList().Count}";
+
+        public string GetSignHoverText(Sign sign, string originalText) => "Online Players";
+    }    
+}
