f757a2ec5a85bca80df210064cc0e0849620114e
jcdcdev <james@jcdc.dev>
2024-09-04 00:34:46 +0100

    feature: current day sign

---
+++ src/jcdcdev.Valheim.Signs/Converters/CurrentDaySign.cs
@@ -0,0 +1,13 @@
+﻿using jcdcdev.Valheim.Signs.Extensions;
+
+namespace jcdcdev.Valheim.Signs.Converters
+{
+    public class CurrentDaySign : IAmADynamicSign
+    {
+        public bool CanConvert(Sign sign, string originalText) => originalText.StartsWith("currentDay", System.StringComparison.InvariantCultureIgnoreCase);
+
+        public string? GetSignText(Sign sign, string originalText) => TimeExtensions.GetCurrentDay();
+
+        public string? GetSignHoverText(Sign sign, string originalText) => "Current Day";
+    }
+}
---
+++ src/jcdcdev.Valheim.Signs/Extensions/TimeExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using HarmonyLib;
+
+namespace jcdcdev.Valheim.Signs.Extensions
+{
+    public static class TimeExtensions
+    {
+        private static float Fraction =>  (float)(AccessTools.Field(typeof(EnvMan), "m_smoothDayFraction")?.GetValue(EnvMan.instance) ?? 0f);
+
+        public static string GetFuzzyTime()
+        {
+            var fuzzyOptions = new[] { "Dawn", "Morning", "Noon", "Afternoon", "Dusk", "Night" };
+            var index = Math.Min((int)(fuzzyOptions.Length * Fraction), fuzzyOptions.Length - 1);
+            var fuzzyTime = fuzzyOptions[index];
+            return $"{fuzzyTime}";
+        }
+
+        public static string GetCurrentDay()
+        {
+            var currentDay = EnvMan.instance.GetCurrentDay();
+            return $"{currentDay}";
+        }
+
+        public static string GetCurrentTimeString(string? format = "HH:mm")
+        {
+            var hour = (int)(Fraction * 24f);
+            var minute = (int)((Fraction * 24f - hour) * 60f);
+            var second = (int)(((Fraction * 24f - hour) * 60f - minute) * 60f);
+
+            var now = DateTime.Now;
+            var dateTimeNow = new DateTime(now.Year, now.Month, now.Day, hour, minute, second);
+            return dateTimeNow.ToString(format);
+        }
+    }
+}
