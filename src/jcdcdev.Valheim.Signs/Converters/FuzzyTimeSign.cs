1643bde68f563d44b0d1af3c62673465b267204d
jcdcdev <james@jcdc.dev>
2024-09-04 00:36:10 +0100

    feature: fuzzy time sign (e.g Morning, Afternoon, Evening)

---
+++ src/jcdcdev.Valheim.Signs/Converters/FuzzyTimeSign.cs
@@ -0,0 +1,13 @@
+﻿using jcdcdev.Valheim.Signs.Extensions;
+
+namespace jcdcdev.Valheim.Signs.Converters
+{
+    public class FuzzyTime : IAmADynamicSign
+    {
+        public bool CanConvert(Sign sign, string originalText) => originalText.StartsWith("fuzzyTime", System.StringComparison.InvariantCultureIgnoreCase);
+
+        public string? GetSignText(Sign sign, string originalText) => TimeExtensions.GetFuzzyTime();
+
+        public string? GetSignHoverText(Sign sign, string originalText) => "Fuzzy Time";
+    }
+}
