7fa0d8ba79f3038aad0d02ca948231fc3034177e
jcdcdev <james@jcdc.dev>
2024-09-05 08:12:22 +0100

    fix: comfort sign

--- src/jcdcdev.Valheim.Signs/Converters/ComfortSign.cs
+++ src/jcdcdev.Valheim.Signs/Converters/ComfortSign.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using jcdcdev.Valheim.Signs.Extensions;
 
 namespace jcdcdev.Valheim.Signs.Converters
 {
     public class ComfortSign : IAmADynamicSign
     {
-        public bool CanConvert(Sign sign, string originalText) => originalText.Equals("comfort", StringComparison.InvariantCultureIgnoreCase);
-
+        public bool CanConvert(Sign sign, string originalText) => Player.m_localPlayer != null && originalText.InvariantEquals("comfort");
+        
         public string? GetSignText(Sign sign, string originalText) => Player.m_localPlayer?.GetComfortLevel().ToString();
 
         public string? GetSignHoverText(Sign sign, string originalText) => "Comfort";
