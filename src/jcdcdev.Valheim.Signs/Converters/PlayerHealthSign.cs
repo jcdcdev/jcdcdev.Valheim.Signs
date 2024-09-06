f1c283f58f395e9809694e85ee6bbc8900d8ee8c
jcdcdev <james@jcdc.dev>
2024-09-06 09:42:34 +0100

    feature: player health sign

---
+++ src/jcdcdev.Valheim.Signs/Converters/PlayerHealthSign.cs
@@ -0,0 +1,18 @@
+﻿using jcdcdev.Valheim.Signs.Extensions;
+
+namespace jcdcdev.Valheim.Signs.Converters
+{
+    public class PlayerHealthSign : IAmADynamicSign
+    {
+        public bool CanConvert(Sign sign, string originalText) => originalText.InvariantEquals("health");
+
+        public string? GetSignText(Sign sign, string originalText)
+        {
+            var health = Player.m_localPlayer.GetHealth();
+            var max = Player.m_localPlayer.GetMaxHealth();
+            return $"{health:F0}/{max:F0}";
+        }
+
+        public string? GetSignHoverText(Sign sign, string originalText) => "Health";
+    }
+}
