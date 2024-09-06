6cac105a4f5f6494a5cfac5cb33398b94e960998
jcdcdev <james@jcdc.dev>
2024-09-06 09:43:36 +0100

    feature: player stamina sign

---
+++ src/jcdcdev.Valheim.Signs/Converters/PlayerStaminaSign.cs
@@ -0,0 +1,18 @@
+﻿using jcdcdev.Valheim.Signs.Extensions;
+
+namespace jcdcdev.Valheim.Signs.Converters
+{
+    public class PlayerStaminaSign : IAmADynamicSign
+    {
+        public bool CanConvert(Sign sign, string originalText) => originalText.InvariantEquals("stamina");
+
+        public string? GetSignText(Sign sign, string originalText)
+        {
+            var stamina = Player.m_localPlayer.m_stamina;
+            var max = Player.m_localPlayer.m_maxStamina;
+            return $"{stamina:F0}/{max:F0}";
+        }
+
+        public string? GetSignHoverText(Sign sign, string originalText) => "Stamina";
+    }
+}
