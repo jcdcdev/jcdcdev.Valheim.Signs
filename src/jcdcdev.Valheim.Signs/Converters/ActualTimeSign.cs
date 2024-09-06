ce64263568e2f4b98272e79290deac886b44d9a4
jcdcdev <james@jcdc.dev>
2024-09-06 09:42:08 +0100

    feature: actual time sign

---
+++ src/jcdcdev.Valheim.Signs/Converters/ActualTimeSign.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using jcdcdev.Valheim.Signs.Extensions;
+
+namespace jcdcdev.Valheim.Signs.Converters
+{
+    public class ActualTimeSign : IAmADynamicSign
+    {
+        public bool CanConvert(Sign sign, string originalText) => originalText.StartsWithInvariant("actualTime");
+
+        public string? GetSignText(Sign sign, string originalText)
+        {
+            var format = TimeExtensions.GetTimeFormat(originalText);
+            return TimeExtensions.GetRealTime(format, TimeZoneInfo.Local);
+        }
+
+        public string? GetSignHoverText(Sign sign, string originalText) => "Actual Time";
+    }
+}
