0d6683453bd8044a40e1068c5d2261d71a630dbb
jcdcdev <james@jcdc.dev>
2025-01-29 00:45:52 +0000

    +changelog-ignore: add JotunnBuildTask as enforced by JotunnLib NuGet package

---
+++ src/JotunnBuildTask/AssemblyPublicizer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Mono.Cecil;
+
+namespace JotunnBuildTask
+{
+    public static class AssemblyPublicizer
+    {
+        /// <summary>
+        ///     Publicize a dll
+        /// </summary>
+        /// <param name="input">Input assembly</param>
+        /// <returns></returns>
+        public static bool PublicizeDll(string input, string hash, string publicizedFolder, string ValheimPath)
+        {
+            if (!File.Exists(input))
+            {
+                System.Console.WriteLine($"File {input} not found.");
+                return false;
+            }
+
+            AssemblyDefinition assemblyDefinition;
+
+            try
+            {
+                System.Console.WriteLine($"Publicizing {input}.");
+
+                assemblyDefinition = AssemblyDefinition.ReadAssembly(input);
+                BaseAssemblyResolver assemblyResolver = (BaseAssemblyResolver)assemblyDefinition.MainModule.AssemblyResolver;
+
+                string managedFolder = Path.Combine(ValheimPath, JotunnBuildTask.ValheimData, JotunnBuildTask.Managed);
+                string serverManagedFolder = Path.Combine(ValheimPath, JotunnBuildTask.ValheimServerData, JotunnBuildTask.Managed);
+
+                if (Directory.Exists(managedFolder))
+                {
+                    assemblyResolver.AddSearchDirectory(managedFolder);
+                }
+
+                if (Directory.Exists(serverManagedFolder))
+                {
+                    assemblyResolver.AddSearchDirectory(serverManagedFolder);
+                }
+            }
+            catch (Exception exception)
+            {
+                System.Console.WriteLine($"{exception.Message}");
+                return false;
+            }
+
+            // Get all type definitions
+            var types = GetTypeDefinitions(assemblyDefinition.MainModule);
+
+            var methods = types.SelectMany(x => x.Methods).Where(x => x.IsPublic == false);
+            var fields = types.SelectMany(x => x.Fields).Where(x => x.IsPublic == false);
+            var events = types.SelectMany(x => x.Events);
+
+            foreach (var type in types)
+            {
+                if (type.IsNested)
+                {
+                    type.IsNestedPublic = true;
+                }
+                else
+                {
+                    type.IsPublic = true;
+                }
+            }
+
+            foreach (var method in methods)
+            {
+                method.IsPublic = true;
+            }
+
+            List<string> eventNames = new List<string>();
+            foreach (var ev in events)
+            {
+                eventNames.Add(ev.Name);
+            }
+
+            foreach (var field in fields)
+            {
+                if (!eventNames.Contains(field.Name))
+                {
+                    field.IsPublic = true;
+                }
+            }
+
+            assemblyDefinition.MainModule.Assembly.CustomAttributes.Add(new CustomAttribute(assemblyDefinition.MainModule.ImportReference(typeof(JVLOriginalAssemblyHashAttribute).GetConstructor(new[] { typeof(string) })))
+            {
+                ConstructorArguments = { new CustomAttributeArgument(assemblyDefinition.MainModule.ImportReference(typeof(string)), hash) }
+            });
+
+            var outputFilename = Path.Combine(publicizedFolder, $"{Path.GetFileNameWithoutExtension(input)}_{JotunnBuildTask.Publicized}{Path.GetExtension(input)}");
+
+            try
+            {
+                assemblyDefinition.Write(outputFilename);
+            }
+            catch (Exception exception)
+            {
+                System.Console.WriteLine($"Could not write file {outputFilename}.");
+                System.Console.WriteLine(exception.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        ///     Returns all type definitions for the module
+        /// </summary>
+        /// <param name="moduleDefinition"></param>
+        /// <returns></returns>
+        private static IEnumerable<TypeDefinition> GetTypeDefinitions(ModuleDefinition moduleDefinition)
+        {
+            return GetTypeDefinitionsRecursive(moduleDefinition.Types);
+        }
+
+        /// <summary>
+        ///     Get all type definitions recursive
+        /// </summary>
+        /// <param name="typeDefinitions"></param>
+        /// <returns></returns>
+        private static IEnumerable<TypeDefinition> GetTypeDefinitionsRecursive(IEnumerable<TypeDefinition> typeDefinitions)
+        {
+            if (typeDefinitions?.Count() == 0)
+            {
+                return new List<TypeDefinition>();
+            }
+
+            return typeDefinitions.Concat(GetTypeDefinitionsRecursive(typeDefinitions.SelectMany(x => x.NestedTypes)));
+        }
+    }
+}
---
+++ src/JotunnBuildTask/JotunnBuildTask.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using Mono.Cecil;
+using Microsoft.Build.Framework;
+
+namespace JotunnBuildTask
+{
+    public class JotunnBuildTask : Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public string ValheimPath { get; set; }
+
+        internal const string ValheimServerData = "valheim_server_Data";
+        internal const string ValheimData = "valheim_Data";
+        internal const string Managed = "Managed";
+        internal const string UnstrippedCorlib = "unstripped_corlib";
+        internal const string PublicizedAssemblies = "publicized_assemblies";
+        internal const string Bepinex = "BepInEx";
+        internal const string Plugins = "plugins";
+        internal const string Publicized = "publicized";
+
+        private bool HasSameHash(string assembly, string publicizedAssembly, out string hash)
+        {
+            hash = MD5HashFile(assembly);
+            string publicizedHash = ReadHashFromDll(publicizedAssembly);
+            return hash == publicizedHash;
+        }
+
+        private string ReadHashFromDll(string dllFile)
+        {
+            string result = "";
+
+            if (File.Exists(dllFile))
+            {
+                AssemblyDefinition assemblyDefinition = AssemblyDefinition.ReadAssembly(dllFile);
+
+                foreach (var customAttribute in assemblyDefinition.CustomAttributes)
+                {
+                    if (customAttribute.AttributeType.Name == nameof(JVLOriginalAssemblyHashAttribute))
+                    {
+                        result = customAttribute.ConstructorArguments[0].Value.ToString();
+                        break;
+                    }
+                }
+
+                assemblyDefinition.Dispose();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        ///     Hash file (MD5).
+        /// </summary>
+        /// <param name="filename">filename</param>
+        /// <returns>MD5 hash</returns>
+        internal string MD5HashFile(string filename)
+        {
+            var hash = MD5.Create().ComputeHash(File.ReadAllBytes(filename));
+            return BitConverter.ToString(hash).Replace("-", "");
+        }
+
+        public override bool Execute()
+        {
+            try
+            {
+                // Get managed folder of valheim or valheim_dedicated
+                string managedFolder = string.Empty;
+                if (Directory.Exists(Path.Combine(ValheimPath, ValheimData, Managed)))
+                {
+                    managedFolder = Path.Combine(ValheimPath, ValheimData, Managed);
+                }
+                if (Directory.Exists(Path.Combine(ValheimPath, ValheimServerData, Managed)))
+                {
+                    managedFolder = Path.Combine(ValheimPath, ValheimServerData, Managed);
+                }
+                if (string.IsNullOrEmpty(managedFolder))
+                {
+                    throw new Exception($"{ValheimPath} does not include {ValheimData} or {ValheimServerData}");
+                }
+
+                // Get publicized folder
+                string publicizedFolder = Path.Combine(managedFolder, PublicizedAssemblies);
+                if (!Directory.Exists(publicizedFolder))
+                {
+                    Directory.CreateDirectory(publicizedFolder);
+                }
+
+                List<string> assemblyNames = new List<string>();
+                assemblyNames.AddRange(Directory.GetFiles(managedFolder, "assembly_*.dll"));
+                assemblyNames.AddRange(Directory.GetFiles(managedFolder, "SoftReferenceableAssets.dll"));
+
+                // Loop assemblies and check if the hash has changed
+                foreach (var assembly in assemblyNames)
+                {
+                    var publicizedAssembly = Path.Combine(publicizedFolder, $"{Path.GetFileNameWithoutExtension(assembly)}_{Publicized}{Path.GetExtension(assembly)}");
+
+                    if (!HasSameHash(assembly, publicizedAssembly, out string hash))
+                    {
+                        try
+                        {
+                            // Try to publicize
+                            if (!AssemblyPublicizer.PublicizeDll(assembly, hash, publicizedFolder, ValheimPath))
+                            {
+                                return false;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            System.Console.WriteLine($"Error occured on {assembly}");
+                            System.Console.WriteLine(ex.Message);
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+    }
+}
